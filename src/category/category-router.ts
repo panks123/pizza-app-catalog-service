5217d374b0a50c7cacbbf02774901ae4d153fad5
Pankaj <pankajkumarhzb787@gmail.com>
2024-05-12 01:29:07 +0530

    add async request handler wrapper for error handling

--- src/category/category-router.ts
+++ src/category/category-router.ts
@@ -3,6 +3,7 @@ import { CategoryController } from "./category-controller";
 import categoryValidator from "./category-validator";
 import { CategoryService } from "./category-service";
 import logger from "../config/logger";
+import { asyncWrapper } from "../common/utils/wrapper";
 
 const router = express.Router();
 
@@ -12,5 +13,5 @@ router.get("/", (req, res) => {
 
 const categoryService = new CategoryService();
 const categoryController = new CategoryController(categoryService, logger);
-router.post("/", categoryValidator, categoryController.create);
+router.post("/", categoryValidator, asyncWrapper(categoryController.create));
 export default router;
