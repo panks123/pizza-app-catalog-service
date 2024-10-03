cc2f8fd72ed6eca3c62a62f801b6fbaaabea44df
Pankaj <pankajkumarhzb787@gmail.com>
2024-10-04 00:31:58 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,22 @@ import categoryRouter from "./category/category-router";
 import productRouter from "./product/product-router";
 import cookieParser from "cookie-parser";
 import toppingRouter from "./topping/topping-router";
+import cors from "cors";
+import config from "config";
 
 const app = express();
 
+const ALLOWED_DOMAINS = [
+    String(config.get("frontend.clientUI")),
+    String(config.get("frontend.adminUI")),
+];
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(
+    cors({
+        origin: ALLOWED_DOMAINS,
+    }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
